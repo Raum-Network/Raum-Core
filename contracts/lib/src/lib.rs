c84107844834368d60554c653fd23974d71e4035
RaumTora <tora@raum.network>
2024-10-15 17:06:26 +0530

    using library functions in lib file

--- contracts/lib/src/lib.rs
+++ contracts/lib/src/lib.rs
@@ -1,8 +1,16 @@
 #![no_std]
 
+use soroban_sdk::{
+    contract, contractimpl,
+    Address, Env, Vec, 
+};
+
 mod library;
 mod error;
 mod interface;
 #[cfg(test)]
 mod test;
+
+pub use library::*;
+
 
